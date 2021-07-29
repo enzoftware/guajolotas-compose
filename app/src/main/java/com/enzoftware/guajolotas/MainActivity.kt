fe8ddc8feaede82f67922d50c70de7c7736b8758
Enzo Lizama <lizama.enzo@gmail.com>
2021-07-29 17:10:02 -0500

    Initial commit

---
+++ app/src/main/java/com/enzoftware/guajolotas/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.enzoftware.guajolotas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.enzoftware.guajolotas.ui.theme.GuajolotasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GuajolotasTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    GuajolotasTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/enzoftware/guajolotas/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.enzoftware.guajolotas.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFFA4A0C)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
