77e6fe645658ae059b16904260e73104ed0dcba0
Enzo Lizama <lizama.enzo@gmail.com>
2021-08-12 18:13:22 -0500

    add navigation compoennet

---
+++ app/src/main/java/com/enzoftware/guajolotas/ui/navigation/Navigation.kt
@@ -0,0 +1,22 @@
+package com.enzoftware.guajolotas.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.enzoftware.guajolotas.ui.home.HomeScreen
+import com.enzoftware.guajolotas.ui.splash.SplashScreen
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = NavigationScreens.SPLASH) {
+        composable(route = NavigationScreens.SPLASH) { SplashScreen(navController = navController) }
+        composable(route = NavigationScreens.HOME) { HomeScreen() }
+    }
+}
+
+object NavigationScreens {
+    const val SPLASH = "splash_screen"
+    const val HOME = "home_screen"
+}
