8f0a1efd71c3e949c7223d6d6dcac09bb2337021
Enzo Lizama <lizama.enzo@gmail.com>
2021-08-29 17:23:46 -0500

    add horizontal page

--- app/src/main/java/com/enzoftware/guajolotas/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/enzoftware/guajolotas/ui/detail/DetailScreen.kt
@@ -1,6 +1,7 @@
 package com.enzoftware.guajolotas.ui.detail
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
@@ -20,22 +21,23 @@ import com.enzoftware.guajolotas.domain.models.Product
 import com.enzoftware.guajolotas.ui.components.ProductCounter
 import com.enzoftware.guajolotas.ui.theme.AppColors
 import com.enzoftware.guajolotas.ui.theme.GuajolotasTheme
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 
 @Composable
 fun DetailScreen(onBackPressed: () -> Unit) {
     Scaffold {
-        Column(
-            Modifier.padding(24.dp)
-        ) {
-            DetailAppBar()
-            DetailBody(product = FakeProducts.mockFood)
+        Column(Modifier.padding(24.dp)) {
+            DetailAppBar(onBackPressed)
+            DetailBody(products = FakeProducts.tamales)
         }
     }
 }
 
 
 @Composable
-private fun DetailAppBar() {
+private fun DetailAppBar(onBackPressed: () -> Unit) {
     Row(
         horizontalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier.fillMaxWidth(),
@@ -43,47 +45,58 @@ private fun DetailAppBar() {
     ) {
         Icon(
             painter = painterResource(id = R.drawable.ic_baseline_arrow_back_ios),
-            contentDescription = "Appbar"
+            contentDescription = "Go back arrow",
+            Modifier.clickable { onBackPressed() }
         )
         Icon(
             painter = painterResource(id = R.drawable.shopping_cart),
-            contentDescription = "Appbar"
+            contentDescription = "Go to shopping cart"
         )
     }
 }
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
-fun DetailBody(product: Product) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
-        Image(
-            painter = painterResource(id = product.image),
-            contentDescription = "Product image",
-            Modifier.size(160.dp)
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = product.name, fontSize = 28.sp, fontWeight = FontWeight.W700)
-        Spacer(modifier = Modifier.height(8.dp))
-        Text(
-            text = product.formattedPrice,
-            fontSize = 20.sp,
-            fontWeight = FontWeight.W700,
-            color = AppColors.primary
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        ProductCounter(
-            incrementProductCount = {},
-            decreaseProductCount = {},
-            count = product.quantity
-        )
-        Spacer(modifier = Modifier.height(40.dp))
+fun DetailBody(products: List<Product>) {
+    Column {
+        val pagerState = rememberPagerState(pageCount = products.size, initialOffscreenLimit = 2)
+        val product = products[pagerState.currentPage]
+        HorizontalPager(state = pagerState) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = product.image),
+                    contentDescription = "Product image",
+                    Modifier.size(160.dp)
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(text = product.name, fontSize = 28.sp, fontWeight = FontWeight.W700)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = product.formattedPrice,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.W700,
+                    color = AppColors.primary
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                ProductCounter(
+                    incrementProductCount = {},
+                    decreaseProductCount = {},
+                    count = product.quantity
+                )
+            }
+        }
         Text(
             text = "Sabor",
             fontSize = 20.sp,
             fontWeight = FontWeight.W600,
             textAlign = TextAlign.Start,
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 40.dp, bottom = 24.dp)
         )
-        Spacer(modifier = Modifier.height(24.dp))
     }
 }
 
